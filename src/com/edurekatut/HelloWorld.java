553ecf992f3ad30cf81fd3e5ba8915f0824ebf46
vasilijus <vasiliokas@gmail.com>
2020-06-13 13:54:15 +0100

    static vs nonstatic method

--- src/com/edurekatut/HelloWorld.java
+++ src/com/edurekatut/HelloWorld.java
@@ -2,13 +2,20 @@ package com.edurekatut;
 
 public class HelloWorld {
 
-	public void test() {
+	public void nonStaticTest() {
 		System.out.println("Non static method");
 	}
+	public static void staticTest() {
+		System.out.println("Static method");
+	}
+	
 	public static void main(String[] args) {
 		HelloWorld helloObj = new HelloWorld();
 		System.out.println("hello World!");
 		
-		helloObj.test();
+		helloObj.nonStaticTest();
+		
+		staticTest();
+		
 	}
 }

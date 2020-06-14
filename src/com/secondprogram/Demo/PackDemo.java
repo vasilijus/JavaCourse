083c6f64ca736e84cf02af8a5f5ef748c784139d
vasilijus <vasiliokas@gmail.com>
2020-06-14 21:04:41 +0100

    Seperating , packages\n There are 3 diff ways to use

---
+++ src/com/secondprogram/Demo/PackDemo.java
@@ -0,0 +1,15 @@
+package com.secondprogram.Demo;
+
+//import com.firstprogram.Auto.Vehicle;
+//import com.firstprogram.Auto.Car;
+// OR use
+// import com.firstprogram.*;
+
+
+public class PackDemo {
+//	Vehicle objVeh;
+//	Car objCar;
+	
+	// Or use namespace straight...
+	com.firstprogram.Auto.Car newObjCar;
+}

d1e94860d1214b50c3d5da54bb71ae2549e665ad
vasilijus <vasiliokas@gmail.com>
2020-06-15 23:46:02 +0100

    changed the error to valid expresion

--- src/com/firstprogram/RegexTestingException/ExceptionTest.java
+++ src/com/firstprogram/RegexTestingException/ExceptionTest.java
@@ -5,10 +5,13 @@ import java.util.regex.*;
 public class ExceptionTest {
 
 	public static void main(String [] args) {
+		int denominator = 10;
 		try {
-			System.out.println( 29 / 0);
+			System.out.println("Handle: \n\t" +29 / denominator);
 		} catch(ArithmeticException exception) {
-			System.out.println("Aritmetic Error: "+ exception.getMessage() );
+			System.out.println("Aritmetic Error: \n\t"+ exception.getMessage() );
+		} finally {
+			System.out.println("Try-finally: \n\tBlock continues to excecute");
 		}
 		final String REGEX = "[a-z]+";
 		final String INPUT = "It's a Great Day for Capitalism!";

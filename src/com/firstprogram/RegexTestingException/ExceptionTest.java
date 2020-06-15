fdffe0da4ce3a47d5f5e84fab80031985fff2404
vasilijus <vasiliokas@gmail.com>
2020-06-15 23:41:53 +0100

    Catching Exception ...

--- src/com/firstprogram/RegexTestingException/ExceptionTest.java
+++ src/com/firstprogram/RegexTestingException/ExceptionTest.java
@@ -1,15 +1,21 @@
-package com.firstprogram.RegexTesting;
+package com.firstprogram.RegexTestingException;
 
 import java.util.regex.*;
 
-public class TestRegex {
+public class ExceptionTest {
 
 	public static void main(String [] args) {
+		try {
+			System.out.println( 29 / 0);
+		} catch(ArithmeticException exception) {
+			System.out.println("Aritmetic Error: "+ exception.getMessage() );
+		}
 		final String REGEX = "[a-z]+";
 		final String INPUT = "It's a Great Day for Capitalism!";
 		
 		Pattern pattern = Pattern.compile(REGEX);
 		Matcher matcher = pattern.matcher(INPUT);
+		System.out.println("\nChecking Matched Bits...     ");
 		while( matcher.find() )
 			System.out.println(INPUT.substring(matcher.start(), matcher.end()) );
 		
---
+++ src/com/firstprogram/RegexTestingException/TestRegex.java
@@ -0,0 +1,17 @@
+package com.firstprogram.RegexTestingException;
+
+import java.util.regex.*;
+
+public class TestRegex {
+
+	public static void main(String [] args) {
+		final String REGEX = "[a-z]+";
+		final String INPUT = "It's a Great Day for Capitalism!";
+		
+		Pattern pattern = Pattern.compile(REGEX);
+		Matcher matcher = pattern.matcher(INPUT);
+		while( matcher.find() )
+			System.out.println(INPUT.substring(matcher.start(), matcher.end()) );
+		
+	}
+}

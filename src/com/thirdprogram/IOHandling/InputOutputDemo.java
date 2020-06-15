d96438fa2a4276447b900c7249fb87c29fe91b3c
vasilijus <vasiliokas@gmail.com>
2020-06-16 00:19:53 +0100

    init File Handling

---
+++ src/com/thirdprogram/IOHandling/InputOutputDemo.java
@@ -0,0 +1,11 @@
+package com.thirdprogram.IOHandling;
+
+import java.io.File;
+
+public class InputOutputDemo {
+	
+	public static void main(String[] args) {
+		File file = new File("~/eclipse-workspace/EdurekaTut");
+	}
+
+}

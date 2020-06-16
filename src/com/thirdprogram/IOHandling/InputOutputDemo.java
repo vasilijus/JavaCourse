10e062c7e3114789b65f7a51af65b36545e9fd54
vasilijus <vasiliokas@gmail.com>
2020-06-16 23:12:05 +0100

    reading a file

--- src/com/thirdprogram/IOHandling/InputOutputDemo.java
+++ src/com/thirdprogram/IOHandling/InputOutputDemo.java
@@ -3,18 +3,24 @@ package com.thirdprogram.IOHandling;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class InputOutputDemo {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// ~/eclipse  | for some reason gave error....
 		File empInfoFile = new File("/home/sergio/eclipse-workspace/EdurekaTut/IOFiles/EmployeeInfo.txt");
 		InputOutputDemo demo = new InputOutputDemo();
+
 		demo.writeToFile(empInfoFile);
+
+		File vehicleInfo = new File("/home/sergio/eclipse-workspace/EdurekaTut/IOFiles/VehicleInfo.txt");
+		demo.readFromFile(vehicleInfo);
 	}
 	
 	// good practice create a new method to write...
 	public void writeToFile(File fileToWrite) {
+		// to be able to access it later on ( finally block )
 		PrintWriter empInfoWriter = null;
 		try {
 			empInfoWriter = new PrintWriter(fileToWrite);
@@ -26,6 +32,12 @@ public class InputOutputDemo {
 		} finally {
 			empInfoWriter.close();
 		}
+	}
+
+	public void readFromFile(File fileToRead) throws FileNotFoundException {
+		Scanner scanner = new Scanner(fileToRead);
+		while( scanner.hasNext() )
+			System.out.println("Kube read frin the input file: " + scanner.nextLine());
 	}
 
 }
